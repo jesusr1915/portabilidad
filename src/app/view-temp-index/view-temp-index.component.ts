bb5d3abf199966a2cd87c1f9b83b4b7b9d4e103e
José de Jesús Galvan Rosas <jgalvan2@santander.com.mx>
2018-01-23 21:37:19 -0600

    SE CAMBIA LA RECEPCION DE PARAMETROS PARA EL TOKENSSO

--- src/app/view-temp-index/view-temp-index.component.ts
+++ src/app/view-temp-index/view-temp-index.component.ts
@@ -17,7 +17,7 @@ export class ViewTempIndexComponent implements OnInit {
   }
   onBtnActionClickedV(){
     localStorage.setItem('sessionID', this.sessionValue);
-    this.router.navigate(['/cliente',{token:'TOKEN'}]);
+    this.router.navigate(['/cliente'],{queryParams: {token: 'TOKEN'}});
     //token=Qi7CMDaXY%2Bi324OcY%2BwIg4V50ad7zJ6Gc5AMOV8sySdkb7fbxYh2fRPRE%2BPAfZtjEDE%2FTFGRiLhCNU4CNkgGEK35ngFuu8YvcTRpb%2BaGCa6205cAhUbyYHa24bwNnSBu7YKQx%2BajcETuaXjRnxM5BJBibJeBrePLXq4gY7yQI9hy2KiJ%2B250RADW1khjQ4lU4Js19tLdqWd1Y7NuuxN1XUBeTv%2BIqGAT6H2d4s2%2BB45qXb7V76diGlRdCiwr1Z944OAZAUZKXuliZIeGuZ6e6dMsnyUN0q1vnvX9hMqE%2FHqpu0mkE8iRi%2B9p2MjHAZULz6LjWDOqi%2BsoTlkFIiKQtQ%3D%3D
   }
 
