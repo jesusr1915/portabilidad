8ffce2174653dd47fcfd4236e61beadd78da112a
José de Jesús Galvan Rosas <jgalvan2@santander.com.mx>
2018-02-28 13:27:03 -0600

    FIX DE RECUPERACION DE CUENTA EN CARRUSEL

--- src/app/view-alta-cliente/view-alta-cliente.component.ts
+++ src/app/view-alta-cliente/view-alta-cliente.component.ts
@@ -91,7 +91,7 @@ export class ViewAltaClienteComponent implements OnInit {
       this.tokenUrl = params.token
     });
 
-    localStorage.setItem('backButton', "true");
+    // localStorage.setItem('backButton', "true");
     if(localStorage.getItem('backButton') !== undefined && localStorage.getItem('backButton') !== null){
       if(localStorage.getItem('backButton') !== "true"){
         // console.log("PRIMERA VEZ");
@@ -267,7 +267,7 @@ export class ViewAltaClienteComponent implements OnInit {
   private loadMock(){
     // RECUPERANDO INFORMACION
     this.recoverSavedData("startServices")
-    
+
     // SERVICIO DE SALDOS
     this.loginServices.getSaldosMock()
     .subscribe(
