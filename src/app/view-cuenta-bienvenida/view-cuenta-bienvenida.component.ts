64877b1b896db133ca39cc172f31b7bf7ca47574
José de Jesús Galvan Rosas <jgalvan2@santander.com.mx>
2018-09-20 16:57:07 -0500

    CORRECCIONES SONAR 2

--- src/app/view-cuenta-bienvenida/view-cuenta-bienvenida.component.ts
+++ src/app/view-cuenta-bienvenida/view-cuenta-bienvenida.component.ts
@@ -33,7 +33,7 @@ export class ViewCuentaBienvenidaComponent implements OnInit {
   finalizar(){
     // localStorage.setItem('necesitaPortabilidad', "true"); // SE DESCOMENTA PARA PROBAR EL MENSAJE INICIAR
     if(localStorage.getItem('necesitaPortabilidad') === "true"){
-      this.errorService("","Para disfrutar de los beneficios de Santander Plus es indispensable tener su nómina en Santander, en caso de no tenerla debe contar con uno o varios depósitos al mes en su(s) cuenta(s) Santander.<br/><br/>Traiga su nómina a Santander sin costo ingresando desde el menú lateral a <b>SantaderPlus > Traer la nómina.</b><br/><br/>Por favor cualquier duda o aclaración comuníquese a la línea de<br/>Santander Plus al <br/>01800 0101123.", "Aceptar", "info", 3);
+      this.errorService("","Para disfrutar de los beneficios de Santander Plus es indispensable tener su nómina en Santander, en caso de no tenerla debe contar con uno o varios depósitos al mes en su(s) cuenta(s) Santander.<br/><br/>Traiga su nómina a Santander sin costo ingresando desde el menú lateral a <strong>SantaderPlus > Traer la nómina.</strong><br/><br/>Por favor cualquier duda o aclaración comuníquese a la línea de<br/>Santander Plus al <br/>01800 0101123.", "Aceptar", "info", 3);
       this.subscriptionM = this.alertMan.getMessage()
       .subscribe(
         message => {
@@ -55,7 +55,7 @@ export class ViewCuentaBienvenidaComponent implements OnInit {
         if(res.error.clave == "OK"){
           this.spinnerMng.showSpinner(false);
           if(res.dto.saldoCreciente === ""){
-            this.errorService("","Recibir remuneraciones por el saldo en Dinero Creciente es otro de los beneficios de Santander Plus; contrate sin costo a través de <b>SuperNet > Inversiones > Contratación Dinero Creciente.</b><br/><br/>Por favor cualquier duda o aclaración comuníquese a la línea de <br/>Santander Plus al <br/>01800 0101123.", "Aceptar", "info", 1);
+            this.errorService("","Recibir remuneraciones por el saldo en Dinero Creciente es otro de los beneficios de Santander Plus; contrate sin costo a través de <strong>SuperNet > Inversiones > Contratación Dinero Creciente.</strong><br/><br/>Por favor cualquier duda o aclaración comuníquese a la línea de <br/>Santander Plus al <br/>01800 0101123.", "Aceptar", "info", 1);
           } else {
             let element: HTMLElement = document.getElementById("goRoot") as HTMLElement;
             element.click();
